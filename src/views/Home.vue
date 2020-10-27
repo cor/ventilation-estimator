19e3b9a24872565b1f66138fb6ca4bdc118c5ecb
cor <cor@pruijs.nl>
2020-10-28 00:17:48 +0100

    Calculate ventilation rate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,32 +2,56 @@
   <div class="home">
     <h1>Ventilation Rate Estimator</h1>
     <div>
-      <label for="">CO<sub>2</sub> Outdoors in ppm</label>
-      <input type="number" v-model.number="outdoorsPpm"><span>ppm</span>
+      <label for="outdoorsPpm">CO<sub>2</sub> Outdoors = </label>
+      <input id="outdoorsPpm" type="number" v-model.number="outdoorsPpm"><span> ppm</span>
     </div>
     <div>
-      <label for="">CO<sub>2</sub> Indoors at peak in ppm</label>
-      <input type="text" v-model.number="indoorsPeakPpm"><span>ppm</span>
+      <label for="indoorsPeakPpm">CO<sub>2</sub> Indoors at peak = </label>
+      <input id="indoorsPeakPpm" type="number" v-model.number="indoorsPeakPpm"><span> ppm</span>
     </div>
     <div>
-      <p>Excess CO<sub>2</sub>: <b>{{excessPpm()}}</b></p>
+      <p>Excess CO<sub>2</sub>: <b>{{excessPpm()}}</b><span> ppm</span></p>
+      <p>Indoors CO<sub>2</sub> at 63% decline: <b>{{Math.floor(declined63Ppm())}}</b> ppm</p>
+    </div>
+    <div>
+      <label for="declined63Time">Time between start and declined to <b>{{Math.floor(declined63Ppm())}}</b> ppm = </label>
+      <input id="declined63Time" type="number" v-model.number="declined63Time"><span> H</span>
+    </div>
+    <div>
+      <p>Ventilation rate = <b>{{ ventilationRate() }}</b> <span> H <sup>-1</sup></span></p>
     </div>
   </div>
 </template>
 
+<style>
+
+input {
+  width: 80px;
+  font-weight: bold;
+}
+</style>
+
+
 <script lang="ts">
 import {Options, Vue} from "vue-class-component";
-
-// @Options({
-//   components: {}
-// })
 
 export default class Home extends Vue {
   public outdoorsPpm = 0;
   public indoorsPeakPpm = 0;
+  public declined63Time = 0;
 
   excessPpm(): number {
     return this.indoorsPeakPpm - this.outdoorsPpm;
+  }
+
+  declined63Ppm(): number {
+    const declineTarget = (100-63)/100;
+    return this.outdoorsPpm + declineTarget * this.excessPpm();
+  }
+
+  ventilationRate(): number {
+    const rate = 1 / this.declined63Time
+    return Math.round( rate * 1000) / 1000;
   }
 }
 </script>
